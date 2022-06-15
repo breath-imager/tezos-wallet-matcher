5b7a24d4cda94b85137a6f9f1e16a7c72536bd91
breath-imager <breathimager@protonmail.com>
2022-06-15 16:54:15 -0400

    merge + compare dataframes

--- main.py
+++ main.py
@@ -20,13 +20,23 @@ cur = conn.cursor()
 
 #RETRIEVE SEASON 0 PASS HOLDERS TEZOS WALLETS
 cur.execute(
-  'SELECT * FROM tezos_wallets'
+  'SELECT tezos_wallet FROM tezos_wallets'
 )
 record = cur.fetchall()
-season0_df = pd.DataFrame(record)
-print(season0_df.head())
+season0_df = pd.DataFrame(record, columns=['wallet_address'])
+
 
 # RETRIEVE OFFERS FOR GIVEN NFT
+# query MyQuery {
+#  offer(where: {fa_contract: {_eq: "KT1Rxr3aE9f7p2159VQCowzxQTkuc4QR1iu1"}, status: {_eq: "active"}, token: {token_id: {_eq: "3"}}}) {
+#    id
+#    price
+#    buyer_address
+#    buyer {
+#      alias
+#    }
+#  }
+#}
 query = """
 query MyQuery {
   offer(where: {fa_contract: {_eq: "KT1Rxr3aE9f7p2159VQCowzxQTkuc4QR1iu1"}, status: {_eq: "active"}, token: {token_id: {_eq: "3"}}}) {
@@ -36,6 +46,7 @@ query MyQuery {
     buyer {
       alias
     }
+
   }
 }
 """
@@ -44,9 +55,28 @@ url = "https://data.objkt.com/v2/graphql/"
 r = requests.post(url, json={'query': query})
 json_data = r.json()
 df_data = json_data['data']['offer']
-tezos_df = pd.DataFrame(df_data )
+tezos_df = pd.DataFrame(df_data)
+tezos_df.rename(columns = {'buyer_address':'wallet_address'},inplace=True)
 
+#test database with one wallet in common
+#tezos_df = pd.DataFrame({
+#  'wallet_address': ['tz1dqkxxmq2w5g6jzJRndFJY9E3gUdioKYK1','tz1QFffX69UydB6Dfzbn8b5xnUoKsGhtY9e7', 'tz1TWU7UjbF1knsgd4dhkVGPPtnkK4wHukVW']})
+
+
+tezos_df.sort_index(inplace=True)
+print("✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸ offers  ✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸")
 print(tezos_df.head())
+season0_df.sort_index(inplace=True)
+print("✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸ holders wallets ✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸")
+print(season0_df.head(99))
 
+merged = season0_df.merge(tezos_df, indicator=True, how='outer')
+merged[merged['_merge']== 'right_only']
+
+
+merged = merged[merged['_merge'].str.contains('both')]
+merged = pd.DataFrame(merged, columns=['wallet_address'])
+print("✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸ Common wallets ✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸✸")
+print(merged.head(99))
 
 
